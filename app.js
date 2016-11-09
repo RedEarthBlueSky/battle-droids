8115ad09f2cdaca9e227eeb085cd0edb8c6fb976
Ian Salt <ian@isalt.digital>
2016-11-09 20:33:53 +0100

    fix: static files not being served

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
- // function handler to supply to http server
 const app = express();
-const router = require('./router');
+const serv = require('http').Server(app);
 const config = require('./config.json');
 
-app.use('/', router);
+app.get('/', function (req ,res) {
+  res.sendFile(__dirname + '/client/index.html');
+});
 
-app.listen(config.port, function () {
+app.use('/client', express.static(__dirname + '/client'));
+
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/client/404.html');
+});
+
+serv.listen(config.port, function () {
   console.log(`Server running on http://${config.hostname}:${config.port}`);
 });
