35e544202ae8968c8d187e7a038ba8f4b63b0344
Ian Salt <ian@isalt.digital>
2016-11-14 19:59:38 +0100

    feat: seperate player object from socket

--- app.js
+++ app.js
@@ -18,20 +18,30 @@ serv.listen(config.port, function () {
 });
 
 let SOCKET_LIST = {};
+let PLAYER_LIST = {};
+
+let Player = (id) => {
+  var self = {
+    x:50,
+    y:50,
+    rotate:0,
+    id:id,
+  }
+  return self;
+};
 
 const io = require('socket.io')(serv, {});
 io.sockets.on('connection', function(socket){
   console.log('socket connection');
   socket.id = Math.random();
-  socket.x = 0;
-  socket.y = 0;
-  socket.rotate = 0;
   SOCKET_LIST[socket.id] = socket;
 
-  //  if leaver server disconnect session.  Also works
-  //  on page refresh...
+  let player = Player(socket.id);
+  PLAYER_LIST[socket.id] = player;
+
   socket.on('disconnect', function () {
     delete SOCKET_LIST[socket.id];
+    delete PLAYER_LIST[socket.id];
   });
 });
 
@@ -40,17 +50,19 @@ const FRAMES_PER_SECOND = 45;
 let frameTime = 1000/FRAMES_PER_SECOND;
 setInterval(function () {
   let pack = [];  //  package to update all players
-  for (var i in SOCKET_LIST) {
-    var socket = SOCKET_LIST[i];
-    socket.x++;
-    socket.y++;
-    socket.rotate++;
+  for (var i in PLAYER_LIST) {
+    var player = PLAYER_LIST[i];
+    player.x++;
+    player.y++;
+    player.rotate++;
     pack.push({
-      x:socket.x,
-      y:socket.y,
-      rotate:socket.rotate
+      x:player.x,
+      y:player.y,
+      rotate:player.rotate
     });
+    //  loop through the sockets and emit the pack for each socket
     //  broadcast fixes issue with not update but also makes very buggy
+    let socket = SOCKET_LIST[i];
     socket.broadcast.emit('newPosition', pack);
   }
 }, frameTime );
