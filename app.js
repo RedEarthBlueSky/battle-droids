fa918435759a95fb9b298a8200604110a1dc95f7
Ian Salt <ian@isalt.digital>
2016-11-14 12:01:38 +0100

    feat:basic multiplayer

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var serv = require('http').Server(app);  // serves files from http
+const express = require('express');
+const app = express();
+const serv = require('http').Server(app);  // serves files from http
 const config = require('./config.json');
 
 app.get('/',function(req, res) {
@@ -16,3 +16,36 @@ app.get('*',function(req, res) {
 serv.listen(config.port, function () {
   console.log(`Server running on http://${config.hostname}:${config.port}`);
 });
+
+let SOCKET_LIST = {};
+
+let io = require('socket.io')(serv,{});
+io.sockets.on('connection', function (socket) {
+  socket.id = Math.random();
+  socket.x = 0;
+  socket.y = 0;
+  socket.number = "" + Math.floor(10 * Math.random());
+  SOCKET_LIST[socket.id] = socket;
+
+  socket.on('disconnect', function () {
+    delete SOCKET_LIST[socket.id];
+  });
+});
+
+setInterval (function () {
+  var pack = [];
+  for (var i in SOCKET_LIST) {
+    let socket = SOCKET_LIST[i];
+    socket.x++;
+    socket.y++;
+    pack.push({
+      x: socket.x,
+      y: socket.y,
+      number: socket.number
+    });
+  }
+  for (var i in SOCKET_LIST) {
+    let socket = SOCKET_LIST[i];
+    socket.emit('newPosition', pack);
+  }
+}, 1000/45);
